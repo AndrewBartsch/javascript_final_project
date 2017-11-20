46431748b4dc3a50be33d9d1847812198ca88f6e
James Sherry <james.m.sherry@googlemail.com>
2017-11-20 00:41:22 +0000

    added my suggestions

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,6 @@
+/* global $, Skycons, axios, google */
+/* eslint-disable no-console */
+
 // get coordinates on Window on load ( pronise )
 // Then call darksky api with coordinates
 // Populate html with data fron dark sky (with the graphics)
@@ -10,163 +13,196 @@
 
 const $weatherSummary = $('#js-weather-summary');
 const $weatherShowcase = $('#js-weather-showcase');
-const $weatherIcon = $('#js-weather-icon');
-const $coffeeOne = $('#js-coffee-one');
-const $coffeeTwo = $('#js-coffee-two');
-const $coffeeThree = $('#js-coffee-three');
+// const $weatherIcon = $('#js-weather-icon');
+// const $coffeeOne = $('#js-coffee-one');
+// const $coffeeTwo = $('#js-coffee-two');
+// const $coffeeThree = $('#js-coffee-three');
+const $coffeeTable = $('#coffee-table tbody');
 
-function handleWeatherData( data ) {
-   console.log( data.currently );
-   const temp = data.currently.temperature;
-   const time = data.currently.time;
-   const summary = data.currently.summary;
-   const icon = data.currently.icon;
-
-   $weatherSummary.text( summary );
-   $weatherShowcase.append(`
-      <dt>
-         Time
-      </dt>
-      <dd>
-         ${new Date( time*1000 )}
-      </dd>
-      `)
-      renderWeatherIcon( icon );
-      renderWeatherTemp( temp );
-   }
-
-   function renderWeatherTemp( temp ) {
-      const celsiusNumber = ( (temp - 32) * 5/9 ).toFixed(2);
-      const celsiusString = `${celsiusNumber} °C`;
-      $weatherShowcase.append(`
+function renderWeatherTemp(temp) {
+  const celsiusNumber = ((temp - 32) / (9 / 5)).toFixed(2);
+  const celsiusString = `${celsiusNumber} °C`;
+  $weatherShowcase.append(`
          <dt>
             Temperature
          </dt>
          <dd>
             ${celsiusString}
          </dd>
-         `)
-      }
+         `);
+}
+function getIconString(icon) {
+  return icon.toUpperCase().replace(/-/g, '_');
+}
 
-      function renderWeatherIcon( icon ) {
-         const  skycons = new Skycons({
-            color: "black"
-         });
-         const iconString = getIconString( icon )
-         skycons.add(document.getElementById("currentWeatherIcon"), Skycons[iconString]);
-         skycons.play();
-      }
+function renderWeatherIcon(icon) {
+  const skycons = new Skycons({
+    color: 'black',
+  });
+  const iconString = getIconString(icon);
+  skycons.add(document.getElementById('currentWeatherIcon'), Skycons[iconString]);
+  skycons.play();
+}
 
-      function getIconString( icon ) {
-         return icon.toUpperCase().replace(/-/g, '_');
-      }
+function handleWeatherData(data) {
+  console.log('currently', data.currently);
+  const {
+    temperature,
+    time,
+    summary,
+    icon,
+  } = data.currently;
 
-      //get location and return a promise
-      function getPosition (options) {
-         return new Promise(function (resolve, reject) {
-            navigator.geolocation.getCurrentPosition(resolve, reject, options);
-         });
-      }
+  $weatherSummary.text(summary);
+  $weatherShowcase.append(`
+    <dt>
+       Time
+    </dt>
+    <dd>
+       ${new Date(time * 1000)}
+    </dd>
+  `);
+  renderWeatherIcon(icon);
+  renderWeatherTemp(temperature);
+}
 
-      // fetch data from darksky
-      function getWeatherData( coordinates ){
-         axios.get(`https://api.darksky.net/forecast/1457d6f1317a446ae14320510b709b71/${coordinates.latitude}, ${coordinates.longitude}`)
-         .then( response => {
-            // render weather Data
-            handleWeatherData( response.data )
-         })
-         .catch( error => {
-            console.error( error );
-         })
-      }
+// get location and return a promise
+function getPosition(options) {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject, options);
+  });
+}
 
-      // start the app
-      document.addEventListener("DOMContentLoaded", ( ) =>  {
-         window.onload = ( ) => {
-            getPosition()
-            .then((response) => {
-               console.log( response.coords.latitude );
-               console.log( response.coords.longitude );
-               latitude = response.coords.latitude;
-               longitude = response.coords.longitude;
-               getWeatherData( response.coords );
-               getCoffeeLocations( response.coords );
-               //initMap( response.coords );
-            })
-            .catch((err) => {
-               console.error(err.message);
-            });
-         }
-      });
+// fetch data from darksky
+function getWeatherData(coordinates) {
+  axios.get(`https://api.darksky.net/forecast/1457d6f1317a446ae14320510b709b71/${coordinates.latitude}, ${coordinates.longitude}`)
+    .then((response) => {
+      // render weather Data
+      handleWeatherData(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
 
-      // fetch coffee venues from Foursquare
+// COFFEE
+function initMap(coordinates, response) {
+  const myLatLng = {
+    lat: coordinates.latitude,
+    lng: coordinates.longitude,
+    info: 'You!',
+  };
+  // Create a map object and specify the DOM element for display.
+  const map = new google.maps.Map(document.getElementById('map'), {
+    center: myLatLng,
+    zoom: 14,
+  });
+  // Create a marker and set its position.
+  // const coffeeShopOne = {
+  //   lat: response.data.response.groups[0].items[0].venue.location.lat,
+  //   lng: response.data.response.groups[0].items[0].venue.location.lng,
+  //   info: response.data.response.groups[0].items[0].venue.name
+  // };
+  //
+  // const coffeeShopTwo = {
+  //   lat: response.data.response.groups[0].items[1].venue.location.lat,
+  //   lng: response.data.response.groups[0].items[1].venue.location.lng,
+  //   info: response.data.response.groups[0].items[1].venue.name
+  // };
+  //
+  // const coffeeShopThree = {
+  //   lat: response.data.response.groups[0].items[2].venue.location.lat,
+  //   lng: response.data.response.groups[0].items[2].venue.location.lng,
+  //   info: response.data.response.groups[0].items[2].venue.name
+  // };
 
-      function getCoffeeLocations( coordinates ){
-         axios.get(`https://api.foursquare.com/v2/venues/explore?client_id=VVO2RYUAX4D455H2LVRIUMOUGUVUK3UY0T0YABYFTERIYMS4&client_secret=20YBURIZDKZJHFIGBEPKYSRRIZWDYXTDBAYWEA1SJW24DHMJ&v=20161016&radius=750&openNow=1&section=coffe&ll=${coordinates.latitude},${coordinates.longitude}`)
-         .then( response => {
-            console.log(coordinates);
-            console.log(response);
-            initMap(coordinates, response);
-            const coffeeOne = response.data.response.groups[0].items[0].venue.name;
-            $coffeeOne.text (coffeeOne);
-         })
-         .catch( error => {
-            console.log( error );
-         })
-    };
+  const markerData = response.data.response.groups[0].items;
+  const markers = markerData.map(marker => ({
+    lat: marker.venue.location.lat,
+    lng: marker.venue.location.lng,
+    info: marker.venue.name,
+  }));
 
-      function initMap( coordinates, response ) {
-         const myLatLng = {
-            lat: coordinates.latitude,
-            lng: coordinates.longitude,
-            info: "You!"
-         };
-         // Create a map object and specify the DOM element for display.
-         const map = new google.maps.Map(document.getElementById('map'), {
-            center: myLatLng,
-            zoom: 14
-         });
-         // Create a marker and set its position.
-        const coffeeShopOne = {lat: response.data.response.groups[0].items[0].venue.location.lat,
-                             lng: response.data.response.groups[0].items[0].venue.location.lng,
-                             info: response.data.response.groups[0].items[0].venue.name
-                            };
+  const locations = [
+    myLatLng,
+    // [coffeeShopOne.info, coffeeShopOne.lat, coffeeShopOne.lng, 1],
+    // [coffeeShopTwo.info, coffeeShopTwo.lat, coffeeShopTwo.lng, 2],
+    // [coffeeShopThree.info, coffeeShopThree.lat, coffeeShopThree.lng, 3]
+  ];
 
-        const coffeeShopTwo = {lat: response.data.response.groups[0].items[1].venue.location.lat,
-                            lng: response.data.response.groups[0].items[1].venue.location.lng,
-                            info: response.data.response.groups[0].items[1].venue.name
-                            };
+  console.log('locations', locations);
 
-        const coffeeShopThree = {lat: response.data.response.groups[0].items[2].venue.location.lat,
-                              lng: response.data.response.groups[0].items[2].venue.location.lng,
-                              info: response.data.response.groups[0].items[2].venue.name
-                              };
+  markers.forEach((marker) => {
+    locations.push(marker);
+  });
 
-        var locations = [
-                         [myLatLng.info, myLatLng.lat, myLatLng.lng, 0],
-                         [coffeeShopOne.info, coffeeShopOne.lat, coffeeShopOne.lng, 1],
-                         [coffeeShopTwo.info, coffeeShopTwo.lat, coffeeShopTwo.lng, 2],
-                         [coffeeShopThree.info, coffeeShopThree.lat, coffeeShopThree.lng, 3]
-                        ];
 
-        var infowindow = new google.maps.InfoWindow({});
+  const infowindow = new google.maps.InfoWindow({});
 
-        var marker, i;
-        var markerLabel = 'X123';
-        var markerIndex = 0;
+  let marker;
 
-        for (i = 0; i < locations.length; i++) {
-		marker = new google.maps.Marker({
-			position: new google.maps.LatLng(locations[i][1], locations[i][2], locations[i][3], locations[i],4),
-			map: map,
-            label: markerLabel[markerIndex++ % markerLabel.length]
-		});
+  for (let i = 0, len = locations.length; i < len; i += 1) {
+    const loc = locations[i];
+    console.log('location', loc);
+    marker = new google.maps.Marker({
+      position: new google.maps.LatLng(loc.info, loc.lat, loc.lng, loc, i),
+      map,
+      label: (i === 0) ? 'X' : i.toString(),
+    });
+    console.log('marker', marker);
 
-        google.maps.event.addListener(marker, 'click', (function (marker, i) {
-			return function () {
-				infowindow.setContent(locations[i][0]);
-				infowindow.open(map, marker);
-			}
-		})(marker, i));
-        };
-    }
+    google.maps.event.addListener(marker, 'click', () => {
+      infowindow.setContent(loc.info);
+      infowindow.open(map, marker);
+    });
+  }
+}
+
+function writeTableRows(items) {
+  items.forEach((item, i) => {
+    if (!i) return;
+    $coffeeTable.append(`<tr>
+      <td>${i}</td>
+      <td>${item.venue.name}</td>
+      <td>
+  ${item.address.map(fragment => `<span>${fragment}</span>`)}
+      </td>
+      <td>${item.rating}</td>
+    </tr>`);
+  });
+}
+
+// fetch coffee venues from Foursquare
+
+function getCoffeeLocations(coordinates) {
+  console.log('coffee coords', coordinates);
+  axios.get(`https://api.foursquare.com/v2/venues/explore?client_id=VVO2RYUAX4D455H2LVRIUMOUGUVUK3UY0T0YABYFTERIYMS4&client_secret=20YBURIZDKZJHFIGBEPKYSRRIZWDYXTDBAYWEA1SJW24DHMJ&v=20161016&radius=750&openNow=1&section=coffe&ll=${coordinates.latitude},${coordinates.longitude}`)
+    .then((response) => {
+      console.log('coffee response ', response);
+      initMap(coordinates, response);
+      const items = response.data.response.groups[0].items[0];
+      // $coffeeOne.text(coffeeOne);
+      writeTableRows(items);
+    })
+    .catch((error) => {
+      console.log('coffee error', error);
+    });
+}
+
+// start the app
+document.addEventListener('DOMContentLoaded', () => {
+  getPosition()
+    .then((response) => {
+      const { coords } = response;
+      const { coords: { longitude: long, latitude: lat } } = response;
+      console.log('lat', lat);
+      console.log('long', long);
+      // latitude = lat;
+      // longitude = long;
+      getWeatherData(coords);
+      getCoffeeLocations(coords);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
